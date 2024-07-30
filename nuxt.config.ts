e69bcfc18b8a997c17e9c8f3fc89cb4cce7efb3a
bino <bonoself@gmail.com>
2024-07-30 21:19:26 +0900

    feat:
    tailwind, srcDir

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+  compatibilityDate: '2024-07-14',
+  ssr: true,
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    '@vueuse/nuxt',
+  ],
+  srcDir: 'src/'
 })
---
+++ src/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="p-4 text-green-600 text-xl">
+    nuxt stated
+  </div>
+</template>
