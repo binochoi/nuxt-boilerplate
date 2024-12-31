0d9e95e619e91bae7227297bb9551e76dd8bd3cd
bino <bonoself@gmail.com>
2025-01-01 01:12:41 +0900

    .

--- server/api/[...auth].ts
+++ server/api/[...auth].ts
@@ -1,6 +1,5 @@
 import { BetterAuth } from 'server/libs/better-auth/better-auth';
-import Config from '../providers/Config';
 
 export default defineEventHandler(
-  (event) => BetterAuth({ env: Config({ event }) }).handler(toWebRequest(event)),
+  (event) => BetterAuth(useRuntimeConfig(event)).handler(toWebRequest(event)),
 );
