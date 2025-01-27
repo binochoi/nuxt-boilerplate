3c0587fe06070cef44ea542a57db7c7939c48114
bino <bonoself@gmail.com>
2025-01-27 21:09:21 +0900

    add env-cmd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
     autoImport: true,
   },
   devServer: {
-    port: Number(process.env.PORT) || 5121,
+    port: Number(process.env.PORT),
     https: {
       cert: './localhost.pem',
       key: './localhost-key.pem',
