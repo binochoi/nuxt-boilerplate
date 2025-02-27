a61a9457f4d8689a52692b1ab019595a1ab7e831
bino <bonoself@gmail.com>
2025-02-27 12:15:44 +0900

    refactor:

--- server/libs/trpc/routers/logo.router.ts
+++ server/libs/trpc/routers/logo.router.ts
@@ -4,11 +4,6 @@ import { procedure, router } from '../trpc';
 import { inject } from '../inject';
 import logoService from '~~/server/services/logo.service';
 import { AuthGuard } from '~~/server/libs/trpc/guards/auth.guard';
-import { UserRepository } from '~~/server/repositories/user.repository';
-import { db } from '../../drizzle/drizzle';
-import { MediaRepository } from '~~/server/repositories/media.repository';
-import R2 from '~~/server/providers/R2';
-import { MediaLinkDownloader } from '~~/server/services/MediaLinkDownloader';
 
 const p = procedure
   .use(
