ddb7ba70eb43b2c84fa3f304f725920438b84296
bino <bonoself@gmail.com>
2025-03-24 19:26:21 +0900

    refactor:

--- app/layers/Archive/apis/useArchiveAPI.ts
+++
@@ -1,7 +0,0 @@
-export const useArchiveAPI = () => {
-  const { api } = useRPC();
-
-  return {
-    getShowcaseList: () => api.archives.$get(),
-  };
-};
--- app/layers/Archive/components/Showcase.vue
+++ app/layers/Archive/components/Showcase.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
-import { useArchiveAPI } from '../apis/useArchiveAPI';
 import List from './Showcase/List.vue';
 
-const { data } = await useAsyncData(async () => useRPC().api.archives.$get());
-const { getShowcaseList } = useArchiveAPI();
 const items = ref<any[]>([]);
 onMounted(async () => {
-  items.value = await getShowcaseList();
+  const { data } = await useAsyncData(async () => useRPC().api.archives.$get());
+  items.value = data.value || [];
 });
 </script>
 
