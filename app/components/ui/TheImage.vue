05722e72ee077e768efbe281f528fd90b006da3f
bino <bonoself@gmail.com>
2025-03-24 23:25:32 +0900

    feat(TheImage): disableAnimation

--- app/components/ui/TheImage.vue
+++ app/components/ui/TheImage.vue
@@ -1,11 +1,16 @@
 <script lang="ts" setup>
 import ImageBase, { type State } from './TheImageBase.vue';
 
-defineProps<{
+const props = defineProps<{
   src?: string,
   isLoading?: boolean,
+  disableAnimation?: boolean,
 }>();
 const state = defineModel<State>('state');
+const imgClass = computed(() => `
+  w-full h-full object-cover
+  ${props.disableAnimation ? '' : 'hover:scale-110 active:scale-110 transition-transform duration-500 animate-in ease-out fade-in fill-mode-backwards'}
+`);
 </script>
 <template>
   <ClientOnly>
@@ -13,7 +18,7 @@ const state = defineModel<State>('state');
       v-model:state="state"
       :src="src || ''"
       class="w-full h-full flex justify-center items-center"
-      imgClass="object-cover w-full h-full hover:scale-110 active:scale-110 transition-transform duration-500 animate-in ease-out fade-in fill-mode-backwards"
+      :imgClass
     >
       <template #loading>
         <TheSpinner
